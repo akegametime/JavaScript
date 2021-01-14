2c5c612787a59b7aac196ab6c0a594324c75cd52
Your Name <you@example.com>
2021-01-14 20:10:05 +0700

    create

---
+++ Datatype.js
@@ -0,0 +1,18 @@
+const people = ["Aaron","Mel","John"];
+const one = 1;
+const str = "hello world ";
+const b = true;
+const employee = {
+    firtst : "anirach ",
+    lastname : "mingkhawn",
+};
+
+function sayHello(person){
+    console.log("hello"+person.firstName);
+
+}
+
+console.log(typeof people);
+console.log(typeof sayhello)
+console.log(employee instanceof array);
+sayhello(employee);
---
+++ case-sens.js
@@ -0,0 +1,7 @@
+const status = 'error';
+
+if (status.toUpperCase() === 'error') {
+    console.log('Something went wrong!');
+} else {
+    console.log('Looks great!!');
+}
